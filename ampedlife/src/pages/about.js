050308156f92242fa521d052368e149b30d94827
N.M. Porterfield <ccmstudiosinc@gmail.com>
2021-05-05 02:40:53 -0400

    services pages

--- ampedlife/src/pages/about.js
+++ ampedlife/src/pages/about.js
@@ -1,13 +1,51 @@
-import React from 'react'
+import React from "react";
+import Amexia from "../assets/images/amexia.jpg";
 
-class About extends React.Component{
-    render(){
-        return(
-            <div>
-<h1>This Is About</h1>
-            </div>
-        )
-    }
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Meet Your Doula</h1>
+        <h2>It's baby time</h2>
+        <div id="aboutmain">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in
+            libero a leo rhoncus tempus. Proin porta ac mauris vel eleifend.
+            Vivamus malesuada, tortor a malesuada vehicula, elit augue tincidunt
+            tellus, vitae tincidunt dui mi sed leo. Vivamus et tincidunt sem.
+            Nullam bibendum vitae ipsum vitae consectetur. Vivamus hendrerit
+            arcu ante, eu mollis libero varius eget. Integer bibendum tristique
+            sem, ac dictum eros facilisis et. Donec sed accumsan velit, sagittis
+            finibus tellus. Sed bibendum mi efficitur feugiat molestie. Proin
+            quis libero ante. Duis cursus mattis sapien, at tincidunt leo
+            consectetur quis. Nunc quis suscipit libero. Morbi quis vulputate
+            mi, ut consequat diam. Praesent imperdiet interdum mauris at
+            laoreet. Integer dictum porttitor nisl non dignissim. Proin mattis
+            tellus nisl, in dapibus lorem eleifend non. Sed molestie magna id
+            orci congue ullamcorper et vel sem. In sed tortor neque. Cras in
+            diam feugiat, fermentum lorem rutrum, faucibus ante. Nunc venenatis
+            iaculis odio quis tristique. Curabitur tempus elit nec dui pharetra
+            sagittis. Aenean libero neque, lacinia id gravida in, interdum id
+            tellus. Aenean erat nulla, malesuada id finibus at, rhoncus vel ex.
+            Morbi bibendum sagittis nisl, in ornare ex maximus nec. Sed vel arcu
+            at augue interdum consectetur quis a ligula. Maecenas vel venenatis
+            leo. Curabitur ac rutrum velit, aliquam feugiat est. Proin non
+            semper lorem, in ullamcorper leo. Nullam aliquam urna diam, in
+            commodo diam fermentum condimentum. Pellentesque habitant morbi
+            tristique senectus et netus et malesuada fames ac turpis egestas.
+            Sed imperdiet venenatis purus id euismod. Suspendisse rhoncus
+            imperdiet dolor, ac dapibus felis congue id. In imperdiet maximus
+            molestie. Nullam posuere sem nisl, id euismod dolor feugiat in.
+            Quisque malesuada et dolor non congue. Sed euismod interdum mauris,
+            eu pharetra nulla. Maecenas sollicitudin urna metus, ac semper neque
+            finibus non. Maecenas quis sapien turpis.
+          </p>
+          <img src={Amexia} alt="Amexia Porterfield" />
+        </div>
+        <a href = 'www.instagram.com'>Schedule today!</a>
+      </div>
+    );
+  }
 }
 
 export default About;
--- ampedlife/src/pages/services.js
+++ ampedlife/src/pages/services.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React from "react";
+import Pregnant from '../assets/images/pregnant.jpg'
 
-class Services extends React.Component{
-    render(){
-        return(
-            <div>
-<h1>This Is Services</h1>
-            </div>
-        )
-    }
+class Services extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This Is Services</h1>
+        <h2>Let's get started</h2>
+        <img src={Pregnant} alt="any day now" />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+          ultricies nibh augue, sit amet semper nulla consequat eget. Aenean vel
+          dignissim ligula, vitae viverra metus. Quisque quis turpis lectus.
+          Quisque lacinia, risus posuere lobortis scelerisque, turpis erat
+          rhoncus mauris, at consectetur turpis nibh ac mi. Ut maximus tellus
+          turpis, eget elementum metus accumsan sed. Duis non tellus semper,
+          viverra massa nec, ullamcorper augue. Duis a magna arcu. Vestibulum
+          scelerisque nibh sit amet elit interdum dignissim. Nulla consectetur
+          quam vitae ipsum tempus tincidunt. In aliquet sodales ex non vehicula.
+          Nullam vitae euismod sem. Maecenas dapibus metus et convallis auctor.
+          Curabitur aliquam luctus massa, eget interdum ex placerat at. Nam
+          faucibus molestie ullamcorper.
+        </p>
+        <hr></hr>
+        <div>
+            <h3>Services</h3>
+            <ul>
+                <li>Service A</li>
+                <li>Service B</li>
+                <li>Service C</li>
+                <li>Service D</li>
+                <li>Service E</li>
+          
+            </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Services;
