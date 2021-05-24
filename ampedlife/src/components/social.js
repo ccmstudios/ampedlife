e0c06c579b352d2b243454a89372b10ef2b270f5
N.M. Porterfield <ccmstudiosinc@gmail.com>
2021-05-24 00:17:58 -0400

    fixed

--- ampedlife/src/components/social.js
+++ ampedlife/src/components/social.js
@@ -10,10 +10,10 @@ class Social extends React.Component{
         return(
             <div id ='social'>
                 <ul>
-                    <li><img src ={Facebook} alt =''/><a href='https://www.facebook.com/ampedlifedoula/' target='_blank' rel="noreferrer"><h3>Facebook</h3></a></li>
-                    <li><img src ={Instagram} alt =''/><a href='https://www.instagram.com/ampedlifedoula/' target='_blank' rel="noreferrer"><h3>Instagram</h3></a></li>
-                    <li><img src ={Twitter} alt =''/><a href='https://twitter.com/AmpedLifeDoula' target='_blank' rel="noreferrer"><h3>Twitter</h3></a></li>
-                    <li><img src ={Pinterest} alt ='https://www.pinterest.com/AMPedLifeDoula/?eq=amped%20life%20d&etslf=4684' target='_blank' rel="noreferrer"/><a href=''><h3>Pinterest</h3></a></li>
+                    <li><img src ={Facebook} alt ='facebook'/><a href='https://www.facebook.com/ampedlifedoula/' target='_blank' rel="noreferrer"><h3>Facebook</h3></a></li>
+                    <li><img src ={Instagram} alt ='insta'/><a href='https://www.instagram.com/ampedlifedoula/' target='_blank' rel="noreferrer"><h3>Instagram</h3></a></li>
+                    <li><img src ={Twitter} alt ='twitter'/><a href='https://twitter.com/AmpedLifeDoula' target='_blank' rel="noreferrer"><h3>Twitter</h3></a></li>
+                    <li><img src ={Pinterest} alt ='pinterest'/><a href='https://www.pinterest.com/AMPedLifeDoula/?eq=amped%20life%20d&etslf=4684' target='_blank' rel="noreferrer"/><a href=''><h3>Pinterest</h3></a></li>
                 
                 </ul>
             </div>
