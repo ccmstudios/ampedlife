3815a166cc3b1cb8bbb80b7bf6f4094a774bc959
N.M. Porterfield <ccmstudiosinc@gmail.com>
2021-05-25 03:45:20 -0400

    none

--- ampedlife/src/components/navbar.js
+++ ampedlife/src/components/navbar.js
@@ -11,33 +11,7 @@ class Navbar extends React.Component{
             <div>
                 <Navigation/>
                 <MobileNav/>
-{/* <nav>
-                 <ul id='navbar'>
-                     <Link style ={{textDecoration: 'none'}} to= '/'>
-                <p><li>Home<li/>
-                     </li></p></Link>
-            <Link style ={{textDecoration: 'none'}} to='/about'>
-                <p><li>About</li></p>
-            </Link>
-            <Link style ={{textDecoration: 'none'}} to='/services'>
-                <p><li>Services</li></p>
-            </Link>
-<Link to='/'>
 
-            <img style ={{zindex:'999'}} src ={Logo} alt ='amped life logo'className ='logo-transparent'/>
-</Link>
-            <Link style ={{textDecoration: 'none'}} to='/blog'>
-                <p><li>Blog</li></p>
-            </Link>
-            <Link style ={{textDecoration: 'none'}} to='/clientportal'>
-                <p><li>Client Portal</li></p>
-            </Link>
-            <Link style ={{textDecoration: 'none'}} to='/contact'>
-                <p><li>Contact</li></p>
-            </Link>
-
-            </ul>
-            </nav> */}
             </div>
         )
     }
