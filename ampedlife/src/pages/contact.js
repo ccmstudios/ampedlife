835710163500f575820264a89ff0927ead68b259
N.M. Porterfield <ccmstudiosinc@gmail.com>
2021-05-25 23:11:13 -0400

    no media queries

--- ampedlife/src/pages/contact.js
+++ ampedlife/src/pages/contact.js
@@ -98,6 +98,19 @@ class Contact extends React.Component {
             <Button onSubmit ={SendEmail} variant="primary" type="submit">
               Submit
             </Button>
+            return (
+    <form className="contact-form" onSubmit={SendEmail}>
+      <input type="hidden" name="contact_number" />
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+
           </Form>
         </div>
       </div>
