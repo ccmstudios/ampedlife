d103baa038b94d3501f6f7cd5c379497f82a5758
N.M. Porterfield <ccmstudiosinc@gmail.com>
2021-06-22 02:48:24 -0400

    trash out

--- ampedlife/src/pages/blog.js
+++ ampedlife/src/pages/blog.js
@@ -1,20 +1,19 @@
-import React from 'react'
+import React from "react";
 
-class Blog extends React.Component{
-    render(){
-        return(
-            <div id='blog'>
-    
-            <iframe
-              src="https://livingtheampedlife.blogspot.com/?fbclid=IwAR2HlyxdJ_SYn6hihPj5i-SCxFrX-FNRaR1Bx145RkA0Yw4_GDrRECJpcOo"
-              title="blog"
-              width="100%"
-              height="1000"
-              frameborder="0"
-            ></iframe>     
-</div>
-        )
-    }
+class Blog extends React.Component {
+  render() {
+    return (
+      <div id="blog">
+        <iframe
+          src="https://livingtheampedlife.blogspot.com/?fbclid=IwAR2HlyxdJ_SYn6hihPj5i-SCxFrX-FNRaR1Bx145RkA0Yw4_GDrRECJpcOo"
+          title="blog"
+          width="100%"
+          height="1000"
+          frameborder="0"
+        ></iframe>
+      </div>
+    );
+  }
 }
 
 export default Blog;
